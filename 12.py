c85a9b5ae90c15f14d9a4efe93f2ea9fdaa52b39
0Bishal7 <sayancr799@gmail.com>
2024-10-16 04:22:19 +0530

    4.21

--- 12.py
+++ 12.py
@@ -1,7 +1,7 @@
 # 12.Create a program that prints the multiplication table of a number ?.
-
 def multiplication_table(n):
-      for i in range(1,11):
-          print(f"{n}*{i}={n}")
-multiplication_table(8)       
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+multiplication_table(5)
+  
 
